b5bc4ce8ccfe017e21d6c4eda897b71c88565b55
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-06-29 16:08:31 +0530

    trying APIs with next, will delte soon

---
+++ app/api/hello/route.ts
@@ -0,0 +1,24 @@
+export const GET = async (request: Request) => {
+  const url = new URL(request.url);
+  //console.log(url);
+  console.log(url.search);
+  console.log(url.searchParams.get('name'));
+
+  return Response.json({
+    message: 'Hello from next',
+  });
+};
+
+export const POST = async (request: Request) => {
+  const url = new URL(request.url);
+  console.log(url.pathname);
+
+  const received = await request.json();
+
+  console.log('recieved body: ', received);
+
+  return Response.json({
+    message: '',
+    body: received,
+  });
+};
